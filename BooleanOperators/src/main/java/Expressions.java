db9360bd8ceedb48c9ea9dbc7afa9c6646f9b181
Katherine Conlan <kdconlan2@gmail.com>
2022-12-28 21:05:48 +0000

    Saved progress on BooleanOperators 2022-12-28 21:05:48 +0000

--- BooleanOperators/src/main/java/Expressions.java
+++ BooleanOperators/src/main/java/Expressions.java
@@ -14,9 +14,11 @@ public class Expressions {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return b;
-        
+        if (b){
+            return b;
+        }
     }
+    
 
     /**
      * @param b a true/false value.
