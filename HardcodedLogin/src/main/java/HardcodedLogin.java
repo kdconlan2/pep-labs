b1c5c023cd79886bb255797c38c676f16f5e3558
Katherine Conlan <kdconlan2@gmail.com>
2023-01-12 05:34:51 +0000

    Saved progress on HardcodedLogin 2023-01-12 05:34:51 +0000

--- HardcodedLogin/src/main/java/HardcodedLogin.java
+++ HardcodedLogin/src/main/java/HardcodedLogin.java
@@ -15,7 +15,11 @@ public class HardcodedLogin {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
-        
+        if (username == "admin")
+            and (password == qwerty){
+                
+            }
+        }
         
         return false;
     }
