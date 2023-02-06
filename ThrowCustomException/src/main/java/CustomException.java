d6630bf833fed678976456f18d4b3cc8c74dfe22
Katherine Conlan <kdconlan2@gmail.com>
2023-02-06 20:25:10 +0000

    Saved progress on ThrowCustomException 2023-02-06 20:25:10 +0000

--- ThrowCustomException/src/main/java/CustomException.java
+++ ThrowCustomException/src/main/java/CustomException.java
@@ -4,5 +4,10 @@
  * By extending Exception, CustomException has inherited all the behavior of Exception while being its own class!
  * Potentially, this class could also override some the behavior of Exception.
  */
+
 public class CustomException extends Exception{
+    public CustomException(String, message);{
+        super ("No Data");
+        this.setCode(code);
+    }
 }
